be8b70d0bb447d434aaad4806c8a5b6a50407d9d
SANGDOLEE <xkdxh625@naver.com>
2024-06-15 02:56:51 +0900

    Feat: CellView 수정

--- Codedrop_Tele/Codedrop_Tele/GoodTaskView .swift
+++ Codedrop_Tele/Codedrop_Tele/GoodTaskView .swift
@@ -194,10 +194,11 @@ struct GridItemView: View {
         }
         .padding()
         .frame(maxWidth: .infinity, minHeight: 180)
+        .background(Color.blue.opacity(0.3))
         .cornerRadius(10)
         .overlay(
             RoundedRectangle(cornerRadius: 10)
-                .stroke(Color.black, lineWidth: 1)
+                .stroke(Color.gray.opacity(0.4), lineWidth: 2)
         )
     }
     
@@ -206,11 +207,13 @@ struct GridItemView: View {
         
         return ZStack {
             Rectangle()
-                .frame(width: 100, height: 22)
-                .foregroundColor(.yellow)
+                .frame(width: 80, height: 22)
+                .foregroundColor(Color.white.opacity(0.3))
+                //.foregroundColor(Color(hex:"#B3E5FF"))
                 .cornerRadius(10)
             
             Text("2024.05.01")
+                .font(.system(size: 14))
                 .multilineTextAlignment(.center)
                 .frame(height: 22, alignment: .center)
         }
@@ -243,7 +246,6 @@ struct GridItemView: View {
     func emojiBadge() -> some View {
         
         return ZStack {
-            
             Text("🎉")
                 .font(.system(size:60))
                 .multilineTextAlignment(.center)
@@ -266,6 +268,33 @@ struct CustomProgressView: View {
                 .scaleEffect(CGSize(width: 1.0, height: 3.0))
                 .padding()
         }
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8 * 4) * 17, (int >> 4 * 4) * 17, (int) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16 * 4, int >> 8 * 4, int)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24 * 4, int >> 16 * 4, int >> 8 * 4, int)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
 
